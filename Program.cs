54f08143b59d41548986146555c27975d8395b53
Elrich De Villiers <Edevilliers9789@conestogac.on.ca>
2021-01-24 22:21:28 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+
+            var stringToReverse = Console.ReadLine();
+
+            var reversedString = "";
+
+            for (int i = stringToReverse.Length - 1; i >= 0; i--)
+            {
+                reversedString = reversedString + stringToReverse[i];
+            }
+
+            Console.WriteLine($"Reversed String is {reversedString}");
+            Console.ReadLine();
+        }
+    }
+}
